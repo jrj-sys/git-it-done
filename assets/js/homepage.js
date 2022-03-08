809786b196a2e60b5888ac0034fa6900528d15b7
jrj-sys <jamesjamel99@gmail.com>
2022-03-08 05:19:09 -0600

    add dynamic fetch request

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,13 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    // Format the GitHub API URL
+    var apiURL = "https://api.github.com/users/" + user + "/repos";
+    // make a request o the url
+    fetch(apiURL).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
-getUserRepos();
+getUserRepos("jrj-sys");
+
+console.log("outside");
