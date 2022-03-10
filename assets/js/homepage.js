cf3fa101fe1a822c71eb58c3f6a51c71a8f0a2b9
jrj-sys <jamesjamel99@gmail.com>
2022-03-10 13:06:35 -0600

    working JS functionality

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+;
 
 
 var formSubmitHandler = function(event) {
